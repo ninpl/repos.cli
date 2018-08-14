4559b66f42c957da454707407c0a16dc9db3d498
Moon Antonio <pinchodev91@gmail.com>
2018-08-14 09:36:44 +0200

    Update menu

---
+++ main/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "************ Elige la categoria ************" << endl;
+    cout << "************ 1->       Unity    ************" << endl;
+    cout << "************ 2->       Unreal   ************" << endl;
+    cout << "************ 3->       C/C++    ************" << endl;
+    cout << "************ 4->       Web      ************" << endl;
+    return 0;
+}
